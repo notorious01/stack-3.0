102b506a4afee8774984d3a3c704bb10b935a865
notorious01 <tuborg1604@gmail.com>
2017-11-06 21:11:19 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,45 +1,20 @@
-#include <stack.hpp>
 #include <catch.hpp>
+#include "stack.hpp"
 
-
-SCENARIO("init", "[init]"){
-	stack<int> st;
-	REQUIRE(st.count() == 0);
+TEST_CASE("push") 
+{
+    stack<int> Stack;
+    Stack.push(5);
+    Stack.push(7);
+    REQUIRE(Stack.count() == 2);
 }
 
-SCENARIO("push", "[push]")  {
-	stack<int> st;
-	st.push(1);
-	st.push(2);
-	REQUIRE(st.count() == 2);
-}
-
-SCENARIO("pop", "[pop]"){
-	stack<int> st;
-	st.push(1);
-	st.push(2);
-	st.pop();
-	REQUIRE(st.count() == 1 );
-}
-
-SCENARIO("top", "[top]") {
-	 stack<int>st;
-    	st.push(1);
-	REQUIRE(st.top() == 1);
-}
-
-SCENARIO("operator=", "[operator]") {
-	stack<int> st1;
-	st1.push(1);
-	st1.push(2);
-	st1.push(3);
-	stack<int> st2 = st1;
-	REQUIRE(st1.count() == st2.count());
-
-}
-
-SCENARIO("empty", "[empty]") {
-	stack<int> st;
-	st.empty();
-	REQUIRE(st.empty() == 1);
+TEST_CASE("pop") 
+{
+    stack<int> Stack;
+    Stack.push(5);
+    Stack.push(7);
+    Stack.push(9);
+    Stack.pop();
+    REQUIRE(Stack.count() == 2);
 }
